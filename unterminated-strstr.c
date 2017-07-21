ee598fa9bc8a20ebf62e502e94cef282675ace98
Ed Maste <emaste@freebsd.org>
2017-07-21 09:57:51 -0400

    unterminated-strstr: be explicit about unmapped page after string
    Previously this example used a single 4k mmap, but provided no guarantee
    that the following page was not mapped (it could be adjacent to an
    existing mapping). Explicitly map an extra page and set it to PROT_NONE.

--- unterminated-strstr.c
+++ unterminated-strstr.c
@@ -7,17 +7,21 @@
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int
 main (int argc, char *argv[])
 {
 	char *buf, *p;
-	const size_t bufsize = 4096;
+	size_t page_size = getpagesize();
+	size_t bufsize = page_size;
 
-	buf = mmap(NULL, bufsize, PROT_READ | PROT_WRITE,
+	buf = mmap(NULL, bufsize + page_size, PROT_READ | PROT_WRITE,
 	    MAP_PRIVATE | MAP_ANON, -1, 0);
 	if (buf == NULL)
 		err(1, "mmap");
+	if (mprotect(buf + bufsize, page_size, PROT_NONE) != 0)
+		err(1, "mprotect");
 	printf("mmap buf=%p\n", buf);
 	memset(buf, 0xff, bufsize);
 
@@ -25,5 +29,4 @@ main (int argc, char *argv[])
 	strncpy(buf, "hello world", 11);
 	p = strstr(buf, "hello");
 	printf("strstr p=%p\n", p);
-	return 0;
 }
